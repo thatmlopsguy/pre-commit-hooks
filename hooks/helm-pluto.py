dbaf340ff3c7585e401014f6196091d0fffbefbc
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-12-08 16:11:42 +0000

    fix: fail fast hooks

--- hooks/helm-pluto.py
+++ hooks/helm-pluto.py
@@ -8,13 +8,17 @@ from pathlib import Path
 
 def check_command_exists(command):
     try:
-        subprocess.run([command], check=True, capture_output=True)
+        subprocess.run([command], capture_output=True)
     except subprocess.CalledProcessError:
         print(f"{command} not installed or available in the PATH", file=sys.stderr)
-        print(f"please check https://helm.sh/docs/intro/install/ for Helm and https://github.com/FairwindsOps/pluto for Pluto", file=sys.stderr)
         sys.exit(1)
 
-def main(charts: str = None):
+def main(charts: str = None, fail_fast: bool = False):
+
+    # Check for required tools
+    check_command_exists('pluto')
+    check_command_exists('helm')
+
     if charts is None:
         # Try to get the chart directory from an environment variable
         charts = os.environ.get('CHART_DIR')
@@ -28,6 +32,7 @@ def main(charts: str = None):
         print(f"Error: Directory '{charts}' does not exist.")
         sys.exit(1)
 
+    fails = 0
     for dir in Path(charts).glob('*'):
         if dir.is_dir():
             dir_name = dir.name
@@ -46,16 +51,21 @@ def main(charts: str = None):
                 pluto_process = subprocess.Popen(pluto_detect_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 output, error = pluto_process.communicate(input=output)
 
-                if output.startswith("There were no resources"):
+                if pluto_process.returncode != 0:
+                    print(f"helm chart {dir_name} needs attention!")
+                    print(output.strip())
+                    if fail_fast:
+                        sys.exit(1)
+                    fails += 1
                     continue
-
-                print(f"helm chart {dir_name}")
-                print(output.strip())
-                sys.exit(1)
 
             except subprocess.SubprocessError as e:
                 print(f"Error running command for {dir_name}: {e}", file=sys.stderr)
                 sys.exit(1)
+
+        if fails > 0:
+            sys.exit(1)
+
 
 if __name__ == "__main__":
     import argparse
